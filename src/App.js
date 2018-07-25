dc932fe2c5a0149bbb54a309f477453f55abffd1
duantao-ds <duantao-ds@gomeplus.com>
2018-07-25 10:05:32 +0800

    初始化项目

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    
+    componentDidMount() {
+        
+        new window.google.maps.Map(this.refs.map, {
+            center: {
+                lat: 39.95934,
+                lng: 116.466588
+            },
+            zoom: 13,
+        })
+        
+    }
+    
+    render() {
+        return (
+            <div className="App">
+                <div ref="map" style={{width: '400px', height: '400px'}}></div>
+            </div>
+        );
+    }
 }
 
 export default App;
