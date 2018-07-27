8ce039408162704dcd586a23cf2ae2553330951f
duantao-ds <duantao-ds@gomeplus.com>
2018-07-27 11:53:57 +0800

    第三方 API 请求成功

--- src/MapPage/MapPage.js
+++ src/MapPage/MapPage.js
@@ -1,12 +1,13 @@
-
 /**
  * @author duantao
  * @file 地图组件
  * @date 2018/7/26
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './MapPage.css';
+// import $ from 'jquery';
+
 
 
 class MapPage extends Component {
@@ -16,22 +17,27 @@ class MapPage extends Component {
         this.state = {
             defaultMapSetting: {
                 center: {
-                    lat: 39.908715,
-                    lng: 116.397389,
+                    lat: 39.90872,
+                    lng: 116.39748,
                 },
-                zoom: 11,
+                zoom: 13,
             },
             defaultMarkerListData: [
-                // 家
-                {title: 'My Home', location: {lat: 39.960276, lng: 116.462679}},
-                // 公司
-                {title: 'My Company', location: {lat: 39.95934, lng: 116.466588}},
                 // 天安门
-                {title: 'Tian An Men', location: {lat: 39.908715, lng: 116.397389}}
+                {title: '天安门', code: 'tiananmen', location: {lat: 39.90872, lng: 116.39748}},
+                // 故宫
+                {title: '故宫', code: 'gugong', location: {lat: 39.916345, lng: 116.397155}},
+                // 颐和园
+                {title: '颐和园', code: 'yiheyuan', location: {lat: 39.999982, lng: 116.275461}},
+                // 圆明园
+                {title: '圆明园', code: 'yuanmingyuan', location: {lat: 40.008098, lng: 116.298215}},
+                // 北京理工大学
+                {title: '北京理工大学', code: 'beijingligongdaxue', location: {lat: 39.964431, lng: 116.310319}},
             ],
             map: null,
             markerList: [],
-            largeInfoWindow: new window.google.maps.InfoWindow()
+            largeInfoWindow: new window.google.maps.InfoWindow(),
+            bounds: new window.google.maps.LatLngBounds(),
         }
     }
     
@@ -47,13 +53,13 @@ class MapPage extends Component {
     
     setMarkerIcon = color => {
         return new window.google.maps.MarkerImage(
-            'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ color +
+            'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + color +
             '|40|_|%E2%80%A2',
             new window.google.maps.Size(21, 34),
             new window.google.maps.Point(0, 0),
             new window.google.maps.Point(10, 34),
-            new window.google.maps.Size(21,34));
-    }
+            new window.google.maps.Size(21, 34));
+    };
     
     // 获取标记列表
     getMarkerList = () => {
@@ -119,20 +125,38 @@ class MapPage extends Component {
     // 设置标记在地图上
     setMarkerInMap = (markerList, map, show = true) => {
         
+        let {bounds} = this.state;
+        
         if (show) {
-            markerList.forEach(marker => marker.setMap(map))
+            markerList.forEach(marker => {
+                marker.setMap(map);
+                bounds.extend(marker.position);
+                map.fitBounds(bounds);
+            })
         }
         else {
             markerList.forEach(marker => marker.setMap(null))
         }
     };
     
+    getPlace = address => {
+        window.$.ajax({
+            url: `http://api.map.baidu.com/telematics/v3/travel_attractions?id=${address}&ak=8RgzSljDxpVDNe1Bal9Sz2VpAK0zcIcL&output=json`,
+            type: "GET",
+            dataType: "jsonp",
+        }).then(res => {
+            console.log('data ===>>> ', res)
+        });
+    };
+    
     
     componentDidMount() {
         let map = this.initMap();
         let markerList = this.getMarkerList();
-    
+        
         this.setMarkerInMap(markerList, map);
+        
+        this.getPlace('beijingligongdaxue')
         
     }
     
@@ -140,6 +164,7 @@ class MapPage extends Component {
         return (
             <div className='map-box'>
                 <div id='map' ref='map'></div>
+                <div id='message'></div>
             </div>
         )
     }
